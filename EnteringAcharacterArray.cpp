9d1877b69baf3a7b2bc188ed7abac0257b9772b8
ShauryaSarswat <ShauryasarswatpersonalAC@gmail.com>
2021-11-28 16:55:08 +0530

    glitch indeference
    indefrence

--- EnteringAcharacterArray.cpp
+++ EnteringAcharacterArray.cpp
@@ -2,19 +2,9 @@
 using namespace std;
 int main()
 {
-    int n;
-    cout<<"Enter the number of character inside the array \n";
-    cin>>n;
-    char arr[n+1];
-    cout<<"Enter the character array \n";
-    // cin>>arr;
-    // cout<<"The character array is "<<arr;
-    int i=0;
-    while(i<20)
-    {
-        cin>>arr[i];
-        i++;
-    }
-    cout<<arr;
-    return 0;
+  int x=2;
+   int *a1=&x;
+   int *a2=&a1;
+   cout<<**a2;
+
 }
